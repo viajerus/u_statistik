0ee298195117282d1fef36dc30c23fa65cfd4eb9
Daniel Abanto Graffman <abanto@stud.uni-heidelberg.de>
2023-11-27 22:42:11 +0100

    add

--- vl_6/uebung6.R
+++ vl_6/uebung6.R
@@ -54,6 +54,33 @@ imputations <- mi(imputations, n.iter = 5)
 
 plot(imputations)
 
+imput.mi.one <- mi::complete(imputations, 1)
+summary(imput.mi.one)
+
+
+#2.2: using mice
+
+imputed.mice.mean <- mice::complete(mice(pima, method="mean", printFlag=F), m=5)[[1]]
+
+imputed.mice.pmm <- mice::complete(mice(pima, method="pmm", printFlag=F), m=5)[[1]]
+
+imputed.mice.rf <- mice::complete(mice(pima, method="rf", printFlag=F), m=5)[[1]]
+
+cor(cbind(pima$insulin, imput.mi.one$insulin, imputed.mice.mean$insulin,
+          imputed.mice.pmm$insulin), use="pairwise.complete.obs")
+
+
+#3 Quantify effect of imputation on model estimates
+
+pima.mice.rf <- mice(pima, method="rf", printFlag=F, m=10)
+fits <- with(data=pima.mice.rf, exp=glm(test ~ pregnant + diastolic + triceps + bmi + age + insulin,
+                                        family=binomial))
+imputed.dats <- mice::complete(pima.mice.rf)
+summary(glm(test ~ pregnant + diastolic + triceps + bmi + age + insulin,
+            family=binomial, data=imputed.dats))
+
+
+mice::pool(fits)
 
 
 
